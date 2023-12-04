5f7120ec7756719a3a27680a8c3453f91dc37dd7
chrismercredi <ckmercredi@icloud.com>
2023-12-03 20:55:03 -0700

    add: env

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'env.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
